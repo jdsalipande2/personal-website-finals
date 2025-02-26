91bbd1af690df6969fd6b5583b5421e944644871
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 08:50:09 +0800

    Update Masthead.vue

---
+++ firstsfc/src/components/Masthead.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="masthead">
+    <div class="video-background">
+      <video autoplay muted loop class="video-bg">
+        <source src="/assets/masthead.mp4" type="video/mp4">
+      </video>
+    </div>
+    <div class="masthead-content">
+      <h1>Hi, I am<br><span class="name">Janice <i>Salipande</i></span></h1>
+      <a href="#portfolio" class="btn-outline">PORTFOLIO</a>
+      <div class="icon-buttons">
+        <a href="https://www.linkedin.com/in/janice-maxene-salipande-8aa856216" target="_blank">
+          <div class="icon-circle">
+            <img src="/assets/linkedin-icon.png" alt="LinkedIn Icon">
+          </div>
+        </a>
+        <a href="mailto:jdsalipande2@student.apc.edu.ph">
+          <div class="icon-circle">
+            <img src="/assets/mail-icon.png" alt="Mail Icon">
+          </div>
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Masthead",
+};
+</script>
+
+<style scoped>
+
+</style>

1d6db71fc83444595ad973a19c15b2fac39b4b6b
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 09:11:45 +0800

    Update About.vue

---
+++ firstsfc/src/components/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="fade-in">
+    <section class="about-section" id="about">
+      <h2>About</h2>
+      <div class="about-content">
+        <div class="about-image">
+          <img src="/assets/Profile%20Picture.jpg" alt="Janice's Portrait" />
+        </div>
+        <div class="about-text">
+          <p><strong>HELLO!</strong><br /><br />
+            I am Janice, a second-year college student studying Computer Science with a focus on Cybersecurity and Forensics. I also do freelance work in graphic design, video editing, and social media management.
+          </p>
+          <p><strong>Hobbies/Interests:</strong><br />
+            - Watching movies, series, and anime<br />
+            - Trying different cuisines<br />
+            - Walking/Jogging in the morning<br />
+            - Freelance Work
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style scoped>
+
+</style>

166e63d4e00f1e80c94333feb77c5e62d2dd5b93
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 10:30:43 +0800

    Update Masthead.vue

--- firstsfc/src/components/Masthead.vue
+++ firstsfc/src/components/Masthead.vue
@@ -11,12 +11,12 @@
       <div class="icon-buttons">
         <a href="https://www.linkedin.com/in/janice-maxene-salipande-8aa856216" target="_blank">
           <div class="icon-circle">
-            <img src="/assets/linkedin-icon.png" alt="LinkedIn Icon">
+            <img src="/assets/linkedin%20icon.png" alt="LinkedIn Icon">
           </div>
         </a>
         <a href="mailto:jdsalipande2@student.apc.edu.ph">
           <div class="icon-circle">
-            <img src="/assets/mail-icon.png" alt="Mail Icon">
+            <img src="/assets/mail%20icon.png" alt="Mail Icon">
           </div>
         </a>
       </div>

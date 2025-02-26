88b8b35d3a2f0013456ea1c73e047d03f448215a
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 14:13:11 +0800

    Update main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,6 +2,7 @@ import './assets/css/styles.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
 // Create and mount the Vue app
 const app = createApp(App);

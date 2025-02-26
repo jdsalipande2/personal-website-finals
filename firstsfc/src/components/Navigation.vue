25d5637c795641439b78c7b3d11d817a88390edd
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 08:48:01 +0800

    Update Navigation.vue

---
+++ firstsfc/src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar">
+    <a href="#" class="logo">
+      <img src="/assets/jmds%20logo%20black%20version.png" alt="My Logo" class="logo-img">
+    </a>
+    <ul class="nav-menu">
+      <li><a href="#about">ABOUT</a></li>
+      <li><a href="#services">SERVICES</a></li>
+      <li><a href="#portfolio">PORTFOLIO</a></li>
+      <li><a href="#contact">CONTACT</a></li>
+      <li><a href="resources.html">RESOURCES</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
+
+<style scoped>
+
+</style>

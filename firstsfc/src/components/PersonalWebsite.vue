442e3c2e535d9d1cc659697cc94e7720172fa911
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 08:38:40 +0800

    Update PersonalWebsite.vue

---
+++ firstsfc/src/components/PersonalWebsite.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Navigation />
+    <Masthead />
+    <About />
+    <Education />
+    <Experience />
+    <Skills />
+    <Services />
+    <Portfolio />
+    <Guestbook />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+import Masthead from "@/components/Masthead.vue";
+import About from "@/components/About.vue";
+import Education from "@/components/Education.vue";
+import Experience from "@/components/Experience.vue";
+import Skills from "@/components/Skills.vue";
+import Services from "@/components/Services.vue";
+import Portfolio from "@/components/Portfolio.vue";
+import Guestbook from "@/components/Guestbook.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Navigation,
+    Masthead,
+    About,
+    Education,
+    Experience,
+    Skills,
+    Services,
+    Portfolio,
+    Guestbook,
+    Footer,
+  },
+};
+</script>

b3f98f43aa6faab735f8784ff2bf78b5553b8181
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 09:16:43 +0800

    Update Experience.vue

---
+++ firstsfc/src/components/Experience.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="fade-in">
+    <section class="experience-section" id="experience">
+      <h2>Experience</h2>
+      <div class="experience-content">
+        <div class="experience-item">
+          <h3>UX/UI DESIGNER</h3>
+          <p>August 2024 - November 2024</p>
+          <p><strong>APC Ramket (E-commerce Website and Mobile App)</strong></p>
+          <p><strong>Asia Pacific College 2nd Year - 1st Term</strong></p>
+          <ul>
+            <li>Led the UX/UI design process for the e-commerce platform.</li>
+            <li>Designed wireframes, mockups, and high-fidelity prototypes for both the website and mobile app, ensuring a consistent user experience across all devices.</li>
+          </ul>
+        </div>
+
+        <div class="experience-item">
+          <h3>VIDEO EDITOR & SOCIAL MEDIA MANAGER</h3>
+          <p>August 2024 - November 2024</p>
+          <p><strong>DreamBuildsLA</strong></p>
+          <ul>
+            <li>Edited Instagram Reels showcasing real estate to captivate audiences and drive inquiries.</li>
+            <li>Managed the Instagram account, ensuring consistent engagement and implementing strategies to grow followers and interactions.</li>
+          </ul>
+        </div>
+
+        <div class="experience-item">
+          <h3>PROJECT DESIGNER</h3>
+          <p>May 2023 - June 2023</p>
+          <p><strong>Marksman Grade Tracking System</strong></p>
+          <p><strong>Asia Pacific College SHS Bootcamp</strong></p>
+          <ul>
+            <li>Conceptualized and developed the idea, focused on streamlining the user experience and meeting project objectives.</li>
+            <li>Created comprehensive project documentation, outlining system processes, design decisions, and technical specifications for future reference and development.</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Experience",
+};
+</script>
+
+<style scoped>
+
+</style>

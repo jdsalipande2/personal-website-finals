b852a69171434932c3bf5455ca6d66d05f7e3c6c
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-26 09:14:39 +0800

    Update Education.vue

---
+++ firstsfc/src/components/Education.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fade-in">
+    <section class="education-section" id="education">
+      <h2>Education</h2>
+      <div class="education-content">
+        <div class="education-card">
+          <div class="education-card-header">
+            <img src="/assets/education%20star%201.png" alt="Asia Pacific College Icon" />
+          </div>
+          <h3>SENIOR HIGH SCHOOL</h3>
+          <p>2021 - 2023</p>
+          <p><strong>Asia Pacific College</strong></p>
+          <p><strong>Awards:</strong></p>
+          <p>With High Honors</p>
+          <p>Most Outstanding Student in Mathematics</p>
+          <p>Most Outstanding Student in Science</p>
+          <p>Meritorious Award in HUMSS</p>
+        </div>
+        
+        <div class="education-card">
+          <div class="education-card-header">
+            <img src="/assets/education%20star%202.png" alt="Asia Pacific College Icon" />
+          </div>
+          <h3>BS COMPUTER SCIENCE</h3>
+          <p>2023 - 2027</p>
+          <p><strong>Asia Pacific College</strong></p>
+          <p><strong>Awards:</strong></p>
+          <p>Dean's Lister (A.Y. 2023 - 2024)</p>
+        </div>
+
+        <div class="education-card">
+          <div class="education-card-header">
+            <img src="/assets/education%20star%203.png" alt="Pasay City National Science High School Icon" />
+          </div>
+          <h3>JUNIOR HIGH SCHOOL</h3>
+          <p>2017 - 2021</p>
+          <p><strong>Pasay City National Science High School</strong></p>
+          <p><strong>Awards:</strong></p>
+          <p>With High Honors</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Education",
+};
+</script>
+
+<style scoped>
+
+</style>

a1aea687ee3a6841b75ae0506e4f9e7d76c80cc5
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-27 12:59:24 +0800

    Update Navigation.vue

--- firstsfc/src/components/Navigation.vue
+++ firstsfc/src/components/Navigation.vue
@@ -8,6 +8,7 @@
       <li><a href="#services">SERVICES</a></li>
       <li><a href="#portfolio">PORTFOLIO</a></li>
       <li><a href="#contact">CONTACT</a></li>
+      <li><a href="guestlist.html">GUEST LIST</a></li>
       <li><a href="resources.html">RESOURCES</a></li>
     </ul>
   </nav>
